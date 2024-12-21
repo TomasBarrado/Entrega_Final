9ac229e8ce3f643b33eab8431d8e7d16c5dddf8a
TomasB <tomibarrado01@gmail.com>
2024-12-21 12:17:31 -0300

    Boton solucionado intento 4

--- veterinaria/settings.py
+++ veterinaria/settings.py
@@ -72,18 +72,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LOGOUT_REDIRECT_URL = 'inicio'
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
-
 STATIC_URL = 'static/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
