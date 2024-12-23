606e50d37f3d7bed6d70d421d9bb7a3d9538454f
TomasBarrado <tomibarrado01@gmail.com>
2024-12-22 23:44:05 -0300

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -14,13 +14,10 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 class UserEditForm(UserCreationForm):
 
-    # Obligatorios
     email = forms.EmailField(label="Ingrese su email:")
     password1 = forms.CharField(label='Contraseña', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Repetir la contraseña', widget=forms.PasswordInput)
-    # No obligatorios
-    # last_name = forms.CharField()
-    # first_name = forms.CharField()
+    
 
     class Meta:
         model = User
@@ -28,8 +25,6 @@ class UserEditForm(UserCreationForm):
             'email',
             'password1',
             'password2',
-            # 'last_name',
-            # 'first_name'
         ]
         help_texts = {k:"" for k in fields}
 
